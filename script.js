8f4140c7b0d8dddd4f8ccfcbe9d54f59fc073555
Glitch (modern-pomodoro) <none>
2018-05-12 15:38:30 +0000

    🏆😇 Checkpoint ./script.js:346598/46

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const timerTextB = document.querySelector("#timer__text-b");
 const timerControlToggle = document.querySelector("#timer-control-toggle");
 const timerControlEdit = document.querySelector("#time-control-edit");
 
+// App settings  
 const defaultRoundTime = 25 * 60; // 25 minutes * 60 seconds
 const defaultBreakTime = 5 * 60; // 5 minutes * 60 seconds
 

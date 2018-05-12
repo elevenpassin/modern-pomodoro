ac9ebed179f9f7b2adf208f9c9171e647a2aaaf4
Glitch (modern-pomodoro) <none>
2018-05-12 17:22:32 +0000

    👜🌆 Checkpoint ./script.js:346598/611

--- script.js
+++ script.js
@@ -4,38 +4,40 @@ const timerTextB = document.querySelector("#timer__text-b");
 const timerControlToggle = document.querySelector("#timer-control-toggle");
 const timerControlEdit = document.querySelector("#time-control-edit");
 
+const SECOND = 1000;
+const MINUTE = SECOND * 60;
 
 class Pomodoro {
   constructor() {
-    this.defaultRoundTime = 10 * 1000; // 25 minutes * 60 seconds * 1000 milliseconds
-    this.defaultBreakTime = 5 * 1000; // 5 minutes * 60 seconds * 1000 milliseconds
+    this.defaultRoundTime = 10 * SECOND; // 25 minutes * 60 seconds * 1000 milliseconds
+    this.defaultBreakTime = 5 * SECOND; // 5 minutes * 60 seconds * 1000 milliseconds
     this.roundTime = this.defaultRoundTime; // Initialize roundTime
     this.breakTime = this.defaultBreakTime; // Initialize breakTime
     this.timerRunning = false;
-    this.isRound = true;
+    this.isRound = false;
     this.timeElapsed = 0;
   }
   
   init(){
     console.log("started");
     this.timerRunning = true;
-    this.startTime = new Date();
     this.timeoutRef = setInterval(() => {
       
       if (this.isRound === true && this.timeElapsed >= this.roundTime) {
-        console.log("round time up yo");
+        
         this.isRound = false;
-        this.startTime = new Date();
+        this.timeElapsed = 0;
       }
       
       if (!this.isRound === true && this.timeElapsed >= this.breakTime) {
-        console.log("break time up yo");
+        
         this.isRound = true;
-        this.startTime = new Date();
+        this.timeElapsed = 0;
       }
-      
-      console.log(this);
-    }, 1000);
+
+      this.timeElapsed += SECOND;
+      timerTextTime.innerText = 
+    }, SECOND);
   }
 }
 

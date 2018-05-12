4ad88d56fea7491288172e50ec6dcc7211abf3b2
Glitch (modern-pomodoro) <none>
2018-05-12 15:28:21 +0000

    🍘🛋 Checkpoint ./script.js:346598/598 ./index.html:346598/22

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-const timerTime = document.querySelector("#timer-time");
-const timerTextA = document.querySelector("#timer-text-a");
-const timerTextB = document.querySelector("#timer-text-b");
+const timerTextTime = document.querySelector("#timer__text-time");
+const timerTextA = document.querySelector("#timer__text-a");
+const timerTextB = document.querySelector("#timer__text-b");
+const timerControlToggle = document.querySelector("#timer-control-toggle");
+const timerControlEdit = document.querySelector("#time-control-edit");
+
+const defaultRoundTime = 25 * 60; // 25 minutes * 60 seconds
+const defaultBreakTime = 5 * 60; // 5 minutes * 60 seconds
 

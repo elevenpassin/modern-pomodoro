b0809c4aba6009bfcb99f7a311b51e4f0f342655
Glitch (modern-pomodoro) <none>
2018-05-13 15:48:51 +0000

    🌕🍗 Checkpoint ./sw.js:346598/128

--- sw.js
+++ sw.js
@@ -3,21 +3,21 @@ importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.2.0/workbox
 if (workbox) {
   workbox.routing.registerRoute(
     new RegExp('^https://fonts.(?:googleapis|gstatic).com/(.*)'),
-    workbox.strategies.staleWhileRevalidate(),
+    workbox.strategies.networkFirst(),
   );
   
   workbox.routing.registerRoute(
     new RegExp('^https://cdn\.glitch\.com/.*'),
-    workbox.strategies.staleWhileRevalidate(),
+    workbox.strategies.networkFirst(),
   );
   
   workbox.routing.registerRoute(
     '/',
-    workbox.strategies.staleWhileRevalidate()
+    workbox.strategies.networkFirst()
   );
   
    workbox.routing.registerRoute(
     /\.(?:js|css|json)$/,
-    workbox.strategies.staleWhileRevalidate()
+    workbox.strategies.networkFirst()
   ); 
 }

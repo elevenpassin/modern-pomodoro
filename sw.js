ba427369c7b9236e2344af8abcd2b3273a3c1b7b
Glitch (modern-pomodoro) <none>
2018-05-13 16:42:53 +0000

    🎤🍖 Checkpoint ./sw.js:346598/128

--- sw.js
+++ sw.js
@@ -3,24 +3,24 @@ importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.2.0/workbox
 if (workbox) {
   workbox.routing.registerRoute(
     new RegExp('^https://fonts.(?:googleapis|gstatic).com/(.*)'),
-    workbox.strategies.networkFirst(),
+    workbox.strategies.cacheFirst(),
   );
   
   workbox.routing.registerRoute(
     new RegExp('^https://cdn\.glitch\.com/.*'),
-    workbox.strategies.networkFirst(),
+    workbox.strategies.cacheFirst(),
   );
   
   workbox.routing.registerRoute(
     '/',
-    workbox.strategies.networkFirst()
+    workbox.strategies.cacheFirst()
   );
   
   workbox.routing.registerRoute(
     // Cache CSS files
     /.*\.css/,
     // Use cache but update in the background ASAP
-    workbox.strategies.staleWhileRevalidate({
+    workbox.strategies.cacheFirst({
       // Use a custom cache name
       cacheName: 'css-cache',
     })
@@ -30,7 +30,7 @@ if (workbox) {
     // Cache JS/JSON files
     /.*\.(js|json)/,
     // Use cache but update in background ASAP
-    workbox.strategies.staleWhileRevalidate({
+    workbox.strategies.cacheFirst({
       cacheName: 'scripts-cache',
     })
   )

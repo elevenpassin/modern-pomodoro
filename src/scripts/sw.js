6a3b2a08bce61a8d34a9e8148cd56b3970872485
buoyantair <buoyantair@protonmail.com>
2018-11-21 22:48:19 +0530

    Add condition for importScripts

--- src/scripts/sw.js
+++ src/scripts/sw.js
@@ -1,4 +1,8 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.2.0/workbox-sw.js');
+if (importScripts) {
+  importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.2.0/workbox-sw.js');
+}
+
+
 
 if (workbox) {
   workbox.routing.registerRoute(
